4bcb5754ececc701c21af7d9198d2c57cfe83f17
INUGYN <116585597+INUGYN@users.noreply.github.com>
2023-07-04 15:51:04 +0200

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,3 +1,18 @@
+@echo off
+
+set "repertoire=%~dp0"
+
+cd %repertoire%/code detecte/dependance
+
+pip install -r requirements.txt
+
+cd ..
+cd ..
+
 python setup.py build
+
+cd %repertoire%/code detecte/dependance
+
+python message.py
 
 exit
