ec604691c0b30c782f2957f9a1dfd9e367580687
tcarpe22 <137800510+tcarpe22@users.noreply.github.com>
2023-07-06 12:08:07 +0200

    Create lo-up-test-X-2.py

---
+++ lo-up-test-X-2.py
@@ -0,0 +1,12 @@
+###Initialisation des plages de couleurs en HSV###
+lower_range_blue = np.array([99, 78, 158])
+upper_range_blue = np.array([113, 156, 227])
+
+lower_range_green = np.array([34, 59, 85])
+upper_range_green = np.array([80, 255, 173])
+
+lower_range_yellow = np.array([0, 121, 116])
+upper_range_yellow = np.array([44, 255, 255])
+
+lower_fish = np.array([0, 0, 0])
+upper_fish = np.array([179, 255, 76])
